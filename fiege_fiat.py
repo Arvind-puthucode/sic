d7d240978505274b3b1a66b34383793fa6ed458f
Arvind-puthucode <62462676+Arvind-puthucode@users.noreply.github.com>
2022-05-16 09:39:05 +0530

    Update fiege_fiat.py

--- fiege_fiat.py
+++ fiege_fiat.py
@@ -1,45 +1,13 @@
+
+  
 import random
-
-def GCD(x, y):
-  
-   while(y):
-       x, y = y, x % y
-  
-   return x
-
-
-
-def PrimeList():
-    i = 0
-    while(i<len(prime_list)):
-        j = i+1
-        while(j<len(prime_list)):
-            if(prime_list[j]%prime_list[i]==0):
-                prime_list.pop(j)
-                j-=1
-            j+=1
-        i+=1
-
-def generatePrime(n):
-    p = random.randrange(2**(n-1)+1, 2**n-1)
-    while(True):
-        flag = False
-        p = random.randrange(2**(n-1)+1, 2**n-1)
-        for i in range(len(prime_list)):
-            if p%prime_list[i]==0:
-                flag = True
-                break
-        if(not flag):
-            return p
-        
-
-prime_list = [i+1 for i in range(1,1000)]
-PrimeList()
+import sympy
+import math
 
 k=3
 
-p = generatePrime(10)
-q = generatePrime(10)
+p = sympy.randprime(2**7,2**8)
+q = sympy.randprime(2**7,2**8)
 n = p * q
 print("P:" , p)
 print("Q:" , q)
@@ -53,7 +21,7 @@ for i in range(k):
     temp1 = None
     while(temp1 == None):
         try:
-            while(GCD(temp,n)!=1):
+            while(math.gcd(temp,n)!=1):
                 temp = random.randrange(2,n-2)
             t = temp**2
             temp1 = pow(t, -1, n)
